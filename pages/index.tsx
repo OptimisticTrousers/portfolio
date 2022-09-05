c5afb178fea902c29a1b63481e8da3855ae12e89
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-05 09:19:16 -0400

    Rendering 'About' in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import About from '../components/About/About'
+import Hero from '../components/Hero/Hero'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -12,6 +14,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
+      <About />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
