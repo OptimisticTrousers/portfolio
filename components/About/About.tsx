11c6bfa52bd5f9588be790da7da9b72affdc37fd
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-05 17:49:34 -0400

    Adding line breaks in the About section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -11,13 +11,20 @@ const About: FC = () => {
         <h2 styleName="about__title">About me</h2>
         <p styleName="about__description">
           Hey! I'm Abdul Rahman, I've been close to a computer since an early
-          age, and been passionate about it ever since. I really liked to build
+          age, and been passionate about it ever since. 
+          
+          <br />
+          <br />
+          I really liked to build
           stuff using no-code tools back in 2010, and from that, I explored how
           to code myself, fast-forward to today, I do programming in various
           languages and technologies, and had the privilege to worked in a B2B
           Company and a Recruitment Company I'm interested in building something
           awesome with code and automate tasks with code, currently focused on
-          Web & Mobile Development, Open Source and Competitive Programming When
+          Web & Mobile Development, Open Source and Competitive Programming. 
+          <br/>
+          <br/>
+          When
           I'm not coding I play games with my friends, watch some show on
           Netflix, or if the weather's good, play basketball! 🏀
         </p>
