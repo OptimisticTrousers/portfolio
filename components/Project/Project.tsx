29ad92e62a1c8442af6cd01f5cf7c7c74e2b4fdc
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-05 20:16:53 -0400

    Adding background-color to projects section

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -12,23 +12,25 @@ const Project: FC = () => {
     <div styleName="project__project">
       <div styleName="project__details">
         <div styleName="project__content">
-          <h2 styleName="project__title">REDDIT CLONE</h2>
-          <i
-            className="devicon-react-original colored"
-            styleName="project__icon"
-          ></i>
-          <i
-            className="devicon-css3-plain colored"
-            styleName="project__icon"
-          ></i>
-          <i
-            className="devicon-javascript-plain colored"
-            styleName="project__icon"
-          ></i>
-          <i
-            className="devicon-jest-plain colored"
-            styleName="project__icon"
-          ></i>
+          <h2 styleName="project__title">Reddit Clone</h2>
+          <div styleName="project__icons">
+            <i
+              className="devicon-react-original colored"
+              styleName="project__icon"
+            ></i>
+            <i
+              className="devicon-css3-plain colored"
+              styleName="project__icon"
+            ></i>
+            <i
+              className="devicon-javascript-plain colored"
+              styleName="project__icon"
+            ></i>
+            <i
+              className="devicon-jest-plain colored"
+              styleName="project__icon"
+            ></i>
+          </div>
           <p styleName="project__description">
             Enim dolor in adipisicing anim pariatur. Ea id quis voluptate Lorem
             ipsum dolore occaecat incididunt adipisicing incididunt dolore sit
@@ -40,27 +42,37 @@ const Project: FC = () => {
           </p>
         </div>
         <div styleName="project__buttons">
-            <Link href="">
-              <div styleName="project__button-container">
-                <Image src="/svg/green-dot.svg" alt="green dot" width={48} height={48} />
-                <button styleName="project__button">LIVE</button>
-              </div>
-            </Link>
-            <Link href="">
-              <div styleName="project__button-container">
-                <Image src="/svg/code.svg" alt="terminal shell" width={48} height={48} />
-                <button styleName="project__button">CODE</button>
-              </div>
-            </Link>
+          <Link href="">
+            <div styleName="project__button-container">
+              <Image
+                src="/svg/green-dot.svg"
+                alt="green dot"
+                width={48}
+                height={48}
+              />
+              <button styleName="project__button">LIVE</button>
+            </div>
+          </Link>
+          <Link href="">
+            <div styleName="project__button-container">
+              <Image
+                src="/svg/code.svg"
+                alt="terminal shell"
+                width={48}
+                height={48}
+              />
+              <button styleName="project__button">CODE</button>
+            </div>
+          </Link>
         </div>
       </div>
-        <Image
-          styleName="project__image"
-          src="/images/app.png"
-          alt="project"
-          width={1250}
-          height={600}
-        />
+      <Image
+        styleName="project__image"
+        src="/images/app.png"
+        alt="project"
+        width={1250}
+        height={600}
+      />
     </div>
   );
 };
