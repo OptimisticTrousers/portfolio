e91a697d68377d6a8989bc92c1ce85ca9bea9bae
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-05 20:28:53 -0400

    Making a footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-unknown-property */
+import { FC } from "react";
+import CSSModules from "react-css-modules";
+import styles from "./Footer.module.css";
+
+const Footer: FC = () => {
+  return (
+    <footer styleName="footer">
+      <p>© 2022 Tony Isern. Made with 💌. Powered by Next.js</p>
+    </footer>
+  );
+};
+
+export default CSSModules(Footer, styles, {
+  allowMultiple: true,
+  handleNotFoundStyleName: "log",
+});
