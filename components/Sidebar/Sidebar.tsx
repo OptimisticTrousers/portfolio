89d4cf89d275dfea6abca17c1145de5b098a3be6
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-09 06:06:07 -0400

    Styling sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -8,10 +8,10 @@ const Sidebar: FC = () => {
   return (
     <aside styleName="aside">
       <div styleName="aside__avatar">
-        <Image src="/images/me.jpg" alt="picture of me" width={100} height={100} />
+        <Image styleName="aside__image" src="/images/me.jpg" alt="picture of me" width={100} height={100} />
       </div>
       <div styleName="aside__content">
-        <h2 styleName="aside__author">Tony Isern</h2>
+        <h3 styleName="aside__author">Tony Isern</h3>
         <p styleName="aside__description">Aspiring full stack web developer. Be different.</p>
       </div>
       <div styleName="aside__buttons">
