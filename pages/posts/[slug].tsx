1310c8f4e8152fa813f110de3585c65b1e7df352
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-09 06:56:58 -0400

    Adding navigation buttons to BlogPost

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,14 +1,36 @@
+/* eslint-disable react/no-unknown-property */
 import { NextPage } from "next";
 import { FC } from "react";
 import CSSModules from "react-css-modules";
 import BlogExcerpt from "../../components/BlogExcerpt/BlogExcerpt";
 import BlogLayout from "../../components/BlogLayout/BlogLayout";
 import styles from "./BlogPost.module.css";
+import { BsCalendar3 } from "react-icons/bs";
+import Link from "next/link";
 
 const BlogPost: NextPage = () => {
   return (
     <BlogLayout>
       <BlogExcerpt />
+      <p styleName="blog__updated-at">
+        <BsCalendar3 />
+        <span>
+          <strong>Updated: </strong>
+          <time>March 01, 2022</time>
+        </span>
+      </p>
+      <nav styleName="blog__pagination">
+        <button styleName="blog__button">
+          <Link href="/">
+            Previous
+          </Link>
+        </button>
+        <button styleName="blog__button">
+          <Link href="/">
+            Next
+          </Link>
+        </button>
+      </nav>
     </BlogLayout>
   );
 };
