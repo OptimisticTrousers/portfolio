03d2e1d82d7d4d64d8843c27b4a43178125566de
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-09 08:41:44 -0400

    Creating a hamburger menu on navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,23 +1,40 @@
 /* eslint-disable react/no-unknown-property */
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useState } from "react";
 import CSSModules from "react-css-modules";
 import styles from "./Navbar.module.css";
+import { FaBars } from "react-icons/fa";
 
 const Navbar: FC = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  function handleMenuClick() {
+    setToggleMenu(prevValue => !prevValue)
+  }
+
   return (
     <header>
       <nav styleName="nav">
         <div styleName="home">
-          <Link href="/" aria-label="home page">Tony Isern</Link>
+          <Link href="/" aria-label="home page">
+            Tony Isern
+          </Link>
         </div>
         <div styleName="pages">
+          <button styleName={`hamburger ${toggleMenu && "active"}`} onClick={handleMenuClick}>
+            <div styleName="bar"></div>
+          </button>
           <ul styleName="list">
-            <li styleName="item" >
-              <Link href="/projects" tabIndex={0} aria-label="projects page">Projects</Link>
+            <li styleName="item">
+              <Link href="/projects" tabIndex={0} aria-label="projects page">
+                Projects
+              </Link>
             </li>
-            <li styleName="item" >
-              <Link href="/contact" tabIndex={0} aria-label="contact page">Contact</Link>
+            <li styleName="item">
+              <Link href="/contact" tabIndex={0} aria-label="contact page">
+                Contact
+              </Link>
             </li>
             <li styleName="item">
               <Link href="/blog">Blog</Link>
