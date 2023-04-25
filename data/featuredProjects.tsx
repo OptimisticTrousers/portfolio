c2a62e0f6aeb39e63f89ccdd4e3a6c50452c36cb
OptimisticTrousers <tony.isern1@gmail.com>
2023-04-24 23:10:43 -0400

    mentioning accessibility in skyfringe description

--- data/featuredProjects.tsx
+++ data/featuredProjects.tsx
@@ -7,7 +7,7 @@ const featuredProjects = [
     id: nanoid(),
     title: "Skyfringe",
     description:
-      "A fullstack social media application built using the MERN stack and Typescript. Front-end application is fully tested with Vitest and React Testing Library. Back-end application is tested with Supertest and mongodb-memory-server. AWS S3 is used to store images. Currently not finished and still in progress.",
+      "A fullstack social media application built using the MERN stack and Typescript. Front-end application is fully tested with Vitest and React Testing Library. Back-end application is tested with Supertest and mongodb-memory-server. AWS S3 is used to store images. Accessibility friendly. Currently not finished and still in progress.",
     codeLink: "https://github.com/OptimisticTrousers/skyfringe",
     liveLink: "https://github.com/OptimisticTrousers/skyfringe",
     media: "/video/skyfringe.webm",
