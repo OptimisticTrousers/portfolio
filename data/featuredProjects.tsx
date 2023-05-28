28f20c99c96975aa2067de31d4b3897c9ee9e115
OptimisticTrousers <tony.isern1@gmail.com>
2023-05-28 16:50:37 -0400

    Making changes to projects

--- data/featuredProjects.tsx
+++ data/featuredProjects.tsx
@@ -7,9 +7,9 @@ const featuredProjects = [
     id: nanoid(),
     title: "Skyfringe",
     description:
-      "A fullstack social media application built using the MERN stack and Typescript. Front-end application is fully tested with Vitest and React Testing Library. Back-end application is tested with Supertest and mongodb-memory-server. AWS S3 is used to store images. Accessibility friendly. Currently not finished and still in progress.",
+      "A fullstack social media application built using the MERN stack and Typescript. Front-end application is fully tested with Vitest and React Testing Library. Back-end application is tested with Supertest and mongodb-memory-server. AWS S3 is used to store images. Accessibility friendly",
     codeLink: "https://github.com/OptimisticTrousers/skyfringe",
-    liveLink: "https://github.com/OptimisticTrousers/skyfringe",
+    liveLink: "https://skyfringe.netlify.app/",
     media: "/video/skyfringe.webm",
     render: () => (
       <>
@@ -83,10 +83,10 @@ const featuredProjects = [
     id: nanoid(),
     title: "Cooking Recipes Application",
     description:
-      "Fullstack web application built with SQLite, React.js and Node.js. Using Material React Table for tabular data. Using several component libraries such as Chakra, Material UI, Mantine, and Bulma for styling. Not yet deployed.",
-    liveLink: "https://github.com/OptimisticTrousers/cooking-recipe-portal",
+      "Fullstack web application built with SQLite, React.js and Node.js. Using Material React Table for tabular data. Using several component libraries such as Chakra, Material UI, Mantine, and Bulma for styling. Deployed API with a Digital Ocean droplet, configured with an NGINX reverse proxy, added an SSL certificate using Let's Encrypt, and served over HTTPS.",
+    liveLink: "https://cooking-recipes-portal.netlify.app/",
     codeLink: "https://github.com/OptimisticTrousers/cooking-recipe-portal",
-    media: "/images/cooking-recipes.png",
+    media: "/video/cooking-recipes.webm",
     render: () => (
       <>
         <i
@@ -112,6 +112,10 @@ const featuredProjects = [
         <i
           className="devicon-javascript-plain colored"
           styleName="project__icon"
+        ></i>
+        <i
+          styleName="project__icon"
+          className="devicon-digitalocean-plain-wordmark colored"
         ></i>
         <i
           className="devicon-materialui-plain colored"
--- data/projects.tsx
+++ data/projects.tsx
@@ -7,9 +7,9 @@ const projects = [
     id: nanoid(),
     title: "Skyfringe",
     description:
-      "A fullstack social media application built using the MERN stack and Typescript. Front-end application is fully tested with Vitest and React Testing Library. Back-end application is tested with Supertest and mongodb-memory-server. AWS S3 is used to store images. Accessibility friendly. Currently not finished and still in progress.",
+      "A fullstack social media application built using the MERN stack and Typescript. Front-end application is fully tested with Vitest and React Testing Library. Back-end application is tested with Supertest and mongodb-memory-server. AWS S3 is used to store images. Accessibility friendly",
     codeLink: "https://github.com/OptimisticTrousers/skyfringe",
-    liveLink: "https://github.com/OptimisticTrousers/skyfringe",
+    liveLink: "https://skyfringe.netlify.app/",
     media: "/video/skyfringe.webm",
     render: () => (
       <>
@@ -83,10 +83,10 @@ const projects = [
     id: nanoid(),
     title: "Cooking Recipes Application",
     description:
-      "Fullstack web application built with SQLite, React.js and Node.js. Using Material React Table for tabular data. Using several component libraries such as Chakra, Material UI, Mantine, and Bulma for styling. Not deployed yet.",
-    liveLink: "https://github.com/OptimisticTrousers/cooking-recipe-portal",
+      "Fullstack web application built with SQLite, React.js and Node.js. Using Material React Table for tabular data. Using several component libraries such as Chakra, Material UI, Mantine, and Bulma for styling. Deployed API with a Digital Ocean droplet, configured with an NGINX reverse proxy, added an SSL certificate using Let's Encrypt, and served over HTTPS.",
+    liveLink: "https://cooking-recipes-portal.netlify.app/",
     codeLink: "https://github.com/OptimisticTrousers/cooking-recipe-portal",
-    media: "/images/cooking-recipes.png",
+    media: "/video/cooking-recipes.webm",
     render: () => (
       <>
         <i
@@ -114,6 +114,10 @@ const projects = [
           styleName="project__icon"
         ></i>
         <i
+          styleName="project__icon"
+          className="devicon-digitalocean-plain-wordmark colored"
+        ></i>
+        <i
           className="devicon-materialui-plain colored"
           styleName="project__icon"
         ></i>
@@ -128,8 +132,8 @@ const projects = [
     id: nanoid(),
     title: "Blog CMS",
     description:
-      "Creating a Blog CMS with React.js and several component libraries. Adding session-based authentication using Passport.js. Creating an CMS for my blog posts, categries, and tags. Fetching content from my blog API to manage my blog through a seperate website. Not deployed yet. ",
-    liveLink: "https://github.com/OptimisticTrousers/blog-cms",
+      "Creating a Blog CMS with React.js and several component libraries. Adding session-based authentication using Passport.js. Creating an CMS for my blog posts, categries, and tags. Fetching content from my blog API to manage my blog through a seperate website. You won't be able to access the CMS because it's only for my personal use. ",
+    liveLink: "https://optimistic-blog-cms.netlify.app/",
     codeLink: "https://github.com/OptimisticTrousers/blog-cms",
     media: "/video/blog-cms.webm",
     render: () => (
